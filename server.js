e4fcd61f37f3f08589fa62327041df36f6217d37
Yotam <yotam.aloni1@gmail.com>
2022-07-24 16:28:59 +0300

    first files commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const expressSession = require("express-session");
+
+const app = express();
+const server = require("http").createServer(app);
+
+// session setup
+const session = expressSession({
+  secret: "coding is amazing",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+});
+
+// Express App Config
+app.use(express.json());
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "public")));
+} else {
+  const corsOptions = {
+    origin: [
+      "http://127.0.0.1:8080",
+      "http://localhost:8080",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+}
+
+const gameRoutes = require("./api/game/game.routes");
+const { connectSockets } = require("./services/socket.service");
+
+app.use("/api/game", gameRoutes);
+connectSockets(server, session);
+
+app.get("/**", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const port = process.env.PORT || 3030;
+server.listen(port, () => {
+  console.log("Server is now running on port " + port);
+});
---
+++ services/socket.service.js
@@ -0,0 +1,53 @@
+
+var gIo = null;
+
+function connectSockets(http, session) {
+  gIo = require("socket.io")(http, {
+    cors: {
+      origin: "*",
+    },
+  });
+  gIo.on("connection", (socket) => {
+    console.log("New socket", socket.id);
+    socket.on("disconnect", (socket) => {
+      console.log("Someone disconnected");
+    });
+
+    socket.on("board-watch", (boardId) => {
+      console.log("board-watch boardId:", boardId);
+
+      if (socket.boardId === boardId) return;
+      if (socket.boardId) {
+        socket.leave(socket.boardId);
+      }
+      socket.join(boardId);
+      socket.boardId = boardId;
+    });
+    socket.on("task-watch", (taskId) => {
+      console.log("board-watch boardId:", taskId);
+
+      if (socket.taskId === taskId) return;
+      if (socket.taskId) {
+        socket.leave(socket.taskId);
+      }
+      socket.join(boardId);
+      socket.boardId = boardId;
+    });
+    
+
+    socket.on("board-update", (board) => {
+      console.log(" board-update boardId:", board._id);
+      console.log("board-update", socket.boardId);
+      socket.to(socket.boardId).emit("board-update", board);
+      // socket.broadcast.emit("board-update", board); //IF ITS ON - SO DONT USE BOARD-WATCH
+    });
+    socket.on("task-update", (task) => {
+      console.log(" task-update taskId:", task.id);
+      socket.broadcast.emit("task-update", task);
+    });
+  });
+}
+
+module.exports = {
+  connectSockets,
+};
